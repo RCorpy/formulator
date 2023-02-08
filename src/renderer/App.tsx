eddf6c3b3cc1796e86ff2abce572111ab58a7305
RCorpym <ramon.navm@gmail.com>
2023-02-08 12:44:12 +0100

    styles for Form, Button and Table installed from bootstrap

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,83 +1,89 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useCallback, useEffect, useState } from "react";
-import icon from '../../assets/icon.svg';
+import React, { useCallback, useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 import './App.css';
-import * as XLSX from 'xlsx'
-
+import * as XLSX from 'xlsx';
 
 function Hello() {
-
-  const fileRoute = "C:/Users/Usuario/Desktop/excels/ENEPOXY W SUELOS 2018 BLANCO   K-1.xls"
+  const fileRoute =
+    'C:/Users/Usuario/Desktop/excels/ENEPOXY W SUELOS 2018 BLANCO   K-1.xls';
 
   const showFile = (file) => {
-    const promise = new Promise((resolve, reject)=>{
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
 
-      const fileReader = new FileReader()
-      fileReader.readAsArrayBuffer(file)
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
 
-      fileReader.onload=(e)=>{
-        const bufferArray = e.target.result
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
 
-        const wb = XLSX.read(bufferArray, {type: "buffer"})
+        const wsname = wb.SheetNames[0];
 
-        const wsname = wb.SheetNames[0]
+        const ws = wb.Sheets[wsname];
 
-        const ws = wb.Sheets[wsname]
+        const data = XLSX.utils.sheet_to_json(ws);
 
-        const data = XLSX.utils.sheet_to_json(ws)
-
-        resolve(data)
-      }
+        resolve(data);
+      };
       fileReader.onerror = (error) => {
-        reject(error)
-      }
-    })
-    promise.then((data)=>console.log(data))
+        reject(error);
+      };
+    });
+    promise.then((data) => console.log(data));
+  };
+
+  function BasicExample() {
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
   }
 
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>hello</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div>
-            <input type="file" onChange={(e) => showFile(e.target.files[0])} />
-          </div>
-        </a>
-      </div>
+      <Form.Group controlId="formFileMultiple" className="mb-3">
+        <Form.Label>Seleccione excel del sistema</Form.Label>
+        <input
+          type="file"
+          onChange={(e) => showFile(e.target.files[0])}
+          multiple
+        />
+      </Form.Group>
+      <Button>{'asdsa'}</Button>
+      {BasicExample()}
     </div>
   );
 }
-
-/*function readXlsx() {
-  XlsxPopulate.fromFileAsync("C:\\Users\\Usuario\\Desktop\\excels\\ENEPOXY W SUELOS 2018 BLANCO   K-1.xls")
-    .then(workbook => {
-        // Modify the workbook.
-        const value = workbook.sheet("Sheet1").cell("A1").value();
- 
-        // Log the value.
-        console.log(value);
-    });
-}*/
 
 export default function App() {
   return (
