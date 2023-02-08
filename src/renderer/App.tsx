cc5a88dd1f10e6ded844538e8820e855a437d074
RCorpy <ramon.navm@gmail.com>
2023-02-09 00:06:39 +0100

    database system introduced, components are in separate files, router works

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,11 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import React, { useCallback, useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import './App.css';
 import * as XLSX from 'xlsx';
+import ProductSelector from './assets/ProductSelector';
 
 function Hello() {
   const fileRoute =
@@ -35,39 +36,7 @@ function Hello() {
     promise.then((data) => console.log(data));
   };
 
-  function BasicExample() {
-    return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-    );
-  }
+  
 
   return (
     <div>
@@ -79,8 +48,10 @@ function Hello() {
           multiple
         />
       </Form.Group>
-      <Button>{'asdsa'}</Button>
-      {BasicExample()}
+      <Link to="/productselector/14">
+      <Button>{'A Product Selector con variable 14'}</Button>
+      </Link>
+      <ProductSelector />
     </div>
   );
 }
@@ -90,6 +61,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/productselector/:id" element={<ProductSelector />} />
       </Routes>
     </Router>
   );
